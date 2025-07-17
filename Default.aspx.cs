fcff216640dcf2b59decfb4a61cc90ec7421270f
Kadir Gündüz <159539238+KadirGunduz13@users.noreply.github.com>
2025-07-17 23:13:04 +0300

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BlogGameProje.Entity;
+
+namespace BlogGameProje
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        BlogOyunEntities1 db = new BlogOyunEntities1();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var bloglar = db.TBLBLOG.ToList();
+                Repeater1.DataSource = bloglar;
+                Repeater1.DataBind();
+            }
+        }
+    }
+}
